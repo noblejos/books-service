f3c6fe737c19c58cf91a872c55d490a1bb01c243
Iroghama Joseph <98827185+noblejos@users.noreply.github.com>
2024-03-05 12:24:57 +0100

    update books controllers

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -16,27 +16,47 @@ export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Post()
-  create(@Body() createBookDto: CreateBookDto) {
-    return this.booksService.create(createBookDto);
+  async create(@Body() createBookDto: CreateBookDto) {
+    const newBook = await this.booksService.create(createBookDto);
+    return {
+      message: 'Books Retrieved Successfully',
+      data: newBook,
+    };
   }
 
   @Get()
-  findAll() {
-    return this.booksService.findAll();
+  async findAll() {
+    const books = await this.booksService.findAll();
+    return {
+      message: 'Books Retrieved Successfully',
+      data: books,
+    };
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.booksService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const book = await this.booksService.findOne(+id);
+    return {
+      message: 'Book Retrieved Successfully',
+      data: book,
+    };
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.booksService.update(+id, updateBookDto);
+  async update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
+    const updatedBook = await this.booksService.update(+id, updateBookDto);
+    return {
+      message: 'Book Updated Successfully',
+      data: updatedBook,
+    };
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.booksService.remove(+id);
+  async remove(@Param('id') id: string) {
+    const deletedBook = await this.booksService.remove(+id);
+    return {
+      message: 'Books Deleted Successfully',
+      data: deletedBook,
+    };
   }
 }
